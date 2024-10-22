fb5087ec4cdb669432c586f7d0b798cc0b93170c
DESKTOP-105S69J\admin <rnjsdn2481@naver.com>
2024-10-22 17:49:51 +0900

    스프링부트 boot01 컨트롤러, 매퍼, 서비스 생성

--- bootworkspace/boot01/src/main/java/com/yedam/app/emp/service/impl/EmpServiceImpl.java
+++ bootworkspace/boot01/src/main/java/com/yedam/app/emp/service/impl/EmpServiceImpl.java
@@ -1,41 +1,63 @@
 package com.yedam.app.emp.service.impl;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.yedam.app.emp.mapper.EmpMapper;
 import com.yedam.app.emp.service.EmpService;
 import com.yedam.app.emp.service.EmpVO;
 
+@Service
 public class EmpServiceImpl implements EmpService{
 
+	private EmpMapper empMapper;
+	
+	//@Autowired : 생성자가 하나만 사용될 경우 생략 가능
+	public EmpServiceImpl(EmpMapper empMapper) {
+		this.empMapper = empMapper;
+	}
+	
 	@Override
 	public List<EmpVO> empList() {
-		// TODO Auto-generated method stub
-		return null;
+		return empMapper.selectAllList();
 	}
 
 	@Override
 	public EmpVO empInfo(EmpVO empVO) {
-		// TODO Auto-generated method stub
-		return null;
+		return empMapper.selectInfo(empVO);
 	}
 
 	@Override
 	public int empInsert(EmpVO empVO) {
-		// TODO Auto-generated method stub
-		return 0;
+		return empMapper.insertInfo(empVO);
 	}
 
 	@Override
 	public Map<String, Object> empUpdate(EmpVO empVO) {
-		// TODO Auto-generated method stub
-		return null;
+		Map<String, Object> map = new HashMap<String, Object>();
+		boolean isSuccessed = false;
+		
+		int result = empMapper.updateInfo(empVO.getEmployeeId(), empVO);
+		
+		if(result == 1) isSuccessed = true;
+		map.put("result", isSuccessed);
+		map.put("target", empVO);
+		// map 은 JSON 형태로 출력됨
+		return map;
 	}
 
 	@Override
 	public Map<String, Object> empDelete(int empId) {
-		// TODO Auto-generated method stub
-		return null;
+		Map<String, Object> map = new HashMap<String, Object>();
+		int result = empMapper.deleteInfo(empId);
+		
+		if (result == 1) map.put("employeeId", empId);
+		
+		return map;
 	}
 
 }
