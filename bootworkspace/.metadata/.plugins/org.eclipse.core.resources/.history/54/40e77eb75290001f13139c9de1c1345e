17c5bd00d9bc44f45e1b3dc2de0e1b3112b716d7
DESKTOP-105S69J\admin <rnjsdn2481@naver.com>
2024-10-22 17:51:43 +0900

    @RequiredArgsContructor 추가

---
+++ bootworkspace/.metadata/.plugins/org.eclipse.core.resources/.history/54/40e77eb75290001f13139c9de1c1345e
@@ -0,0 +1,8 @@
+package com.yedam.app.emp.web;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class EmpController {
+
+}
--- bootworkspace/boot01/src/main/java/com/yedam/app/emp/web/EmpController.java
+++ bootworkspace/boot01/src/main/java/com/yedam/app/emp/web/EmpController.java
@@ -2,7 +2,12 @@ package com.yedam.app.emp.web;
 
 import org.springframework.stereotype.Controller;
 
-@Controller
-public class EmpController {
+import com.yedam.app.emp.service.EmpService;
 
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor // final 선언 필드를 생성자 강제 생성
+public class EmpController {
+	private final EmpService empService;
 }
